b8d2f42511d8eddc09429158577fbf715c64e23b
CHENGSHILIANG046 <CHENGSHILIANG046@pingan.com.cn>
2020-05-20 15:56:39 +0800

    feat: 修复double slider 的倒角问题

--- CSLUILibraryDemo/CSLUILibraryDemo/BasicElement/SliderViewController.m
+++ CSLUILibraryDemo/CSLUILibraryDemo/BasicElement/SliderViewController.m
@@ -9,44 +9,45 @@
 #import "SliderViewController.h"
 
 @interface SliderViewController ()
-@property (nonatomic, weak) IBOutlet SLSliderView *sliderView;
-@property (nonatomic, weak) IBOutlet SLSliderView *verticalSliderView;
+//@property (nonatomic, weak) IBOutlet SLSliderView *sliderView;
+//@property (nonatomic, weak) IBOutlet SLSliderView *verticalSliderView;
 @property (nonatomic, weak) IBOutlet SLDoubleSliderView *doubleSliderView;
-@property (nonatomic, weak) IBOutlet SLDoubleSliderView *verticalDoubleSliderView;
+//@property (nonatomic, weak) IBOutlet SLDoubleSliderView *verticalDoubleSliderView;
 @end
 
 @implementation SliderViewController
 
 - (void)viewDidLoad{
     [super viewDidLoad];
-    self.sliderView.progressView.trackerImage = [UIImage imageNamed:@"3.jpg"];
-    self.sliderView.progressView.normalImage = [UIImage imageNamed:@"cir0.png"];
-    self.sliderView.minValue = 0.2;
-    self.sliderView.maxValue = 0.8;
-    [self.sliderView setProgress:0.1 animated:YES];
-    self.sliderView.progressChange = ^(CGFloat progress) {
-        NSLog(@"%.2lf",progress);
-    };
-    self.verticalSliderView.progressView.trackerImage = [UIImage imageNamed:@"3.jpg"];
-    self.verticalSliderView.progressView.normalImage = [UIImage imageNamed:@"cir0.png"];
-    self.verticalSliderView.vertical = YES;
-    self.verticalSliderView.progressChange = ^(CGFloat progress) {
-        NSLog(@"%.2lf",progress);
-    };
-    self.verticalDoubleSliderView.normalImage = [UIImage imageNamed:@"cir0.png"];
-    self.verticalDoubleSliderView.trackerImage = [UIImage imageNamed:@"3.jpg"];
-    self.verticalDoubleSliderView.vertical = YES;
-    self.verticalDoubleSliderView.progressChange = ^(CGFloat startProgress, CGFloat endProgress) {
-        NSLog(@"%.2lf~~~~~~%.2lf",startProgress, endProgress);
-    };
+//    self.sliderView.progressView.trackerImage = [UIImage imageNamed:@"3.jpg"];
+//    self.sliderView.progressView.normalImage = [UIImage imageNamed:@"cir0.png"];
+//    self.sliderView.minValue = 0.2;
+//    self.sliderView.maxValue = 0.8;
+//    [self.sliderView setProgress:0.1 animated:YES];
+//    self.sliderView.progressChange = ^(CGFloat progress) {
+//        NSLog(@"%.2lf",progress);
+//    };
+//    self.verticalSliderView.progressView.trackerImage = [UIImage imageNamed:@"3.jpg"];
+//    self.verticalSliderView.progressView.normalImage = [UIImage imageNamed:@"cir0.png"];
+//    self.verticalSliderView.vertical = YES;
+//    self.verticalSliderView.progressChange = ^(CGFloat progress) {
+//        NSLog(@"%.2lf",progress);
+//    };
+//    self.verticalDoubleSliderView.normalImage = [UIImage imageNamed:@"cir0.png"];
+//    self.verticalDoubleSliderView.trackerImage = [UIImage imageNamed:@"3.jpg"];
+//    self.verticalDoubleSliderView.vertical = YES;
+//    self.verticalDoubleSliderView.progressChange = ^(CGFloat startProgress, CGFloat endProgress) {
+//        NSLog(@"%.2lf~~~~~~%.2lf",startProgress, endProgress);
+//    };
     self.doubleSliderView.normalImage = [UIImage imageNamed:@"cir0.png"];
     self.doubleSliderView.trackerImage = [UIImage imageNamed:@"3.jpg"];
-    [self.doubleSliderView setStartProgress:0.1 endProgress:0.8];
-    self.doubleSliderView.minValue = 0.2;
-    self.doubleSliderView.maxValue = 0.8;
     self.doubleSliderView.progressChange = ^(CGFloat startProgress, CGFloat endProgress) {
         NSLog(@"%.2lf~~~~~~%.2lf",startProgress, endProgress);
     };
+    [self.doubleSliderView setStartProgress:0.1 endProgress:0.8];
+    self.doubleSliderView.minValue = 0.2;
+    self.doubleSliderView.maxValue = 0.8;
+    
 //    self.doubleSliderView.progressWH = 20;
 //    self.doubleSliderView.slideSize = CGSizeMake(50, 50);
 //    for (UIView *subView in self.doubleSliderView.slideViewArray) {
